db1a433a577daf5a8cfd45e55f14aba04659e1fa
Mateusz Wójciak <matwojciak@gmail.com>
2020-11-05 23:22:22 +0100

    13.1 Movies Map

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        String moviesMap = movieStore.getMovies()
+                .values().stream()
+                .flatMap(n -> n.stream())
+                .collect(Collectors.joining("!", "",""));
+        System.out.println(moviesMap);
+    }
+}
